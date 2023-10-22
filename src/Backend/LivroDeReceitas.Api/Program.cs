a7df3e32371c950dbdb85c5c7567438232387608
Pedro <pedro.henriquedgoncalves@gmail.com>
2023-10-22 20:55:38 -0300

    Adicionando testes de unidade para endpoint registrar usuário.

--- src/Backend/LivroDeReceitas.Api/Program.cs
+++ src/Backend/LivroDeReceitas.Api/Program.cs
@@ -3,6 +3,7 @@ using LivroDeReceitas.Application;
 using LivroDeReceitas.Application.Servicos.Automapper;
 using LivroDeReceitas.Domain.Extension;
 using LivroDeReceitas.Infrastructure;
+using LivroDeReceitas.Infrastructure.AcessoRepositorio;
 using LivroDeReceitas.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -51,10 +52,24 @@ app.Run();
 
 void AtualizarBaseDeDados()
 {
-    var conexao = builder.Configuration.GetConexaoDatabase(); 
-    var nomeDatabase = builder.Configuration.GetNomeDatabase();
-    
-    Database.CriarDatabase(conexao, nomeDatabase);
+    using var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
 
-    app.MigrateBancoDeDados();
+    using var context = serviceScope.ServiceProvider.GetService<LivroDeReceitasContext>();
+
+    bool? databaseInMemory = context?.Database?.ProviderName?.Equals("Microsoft.EntityFrameworkCore.InMemory");
+
+    if(!databaseInMemory.HasValue || !databaseInMemory.Value)
+    {
+        var conexao = builder.Configuration.GetConexaoDatabase();
+        var nomeDatabase = builder.Configuration.GetNomeDatabase();
+
+        Database.CriarDatabase(conexao, nomeDatabase);
+
+        app.MigrateBancoDeDados();
+    }   
+}
+
+public partial class Program
+{
+
 }
---
+++ tests/WebApi.Test/LivroDeReceitasWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using LivroDeReceitas.Infrastructure.AcessoRepositorio;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace WebApi.Test;
+
+public class LivroDeReceitasWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{ 
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var descritor = services.SingleOrDefault(d => d.ServiceType == typeof(LivroDeReceitasContext));
+
+                if(descritor != null)
+                    services.Remove(descritor);
+
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<LivroDeReceitasContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                using var scope = serviceProvider.CreateScope();
+                var scopeService = scope.ServiceProvider;
+                var database = scopeService.GetRequiredService<LivroDeReceitasContext>();
+
+                database.Database.EnsureDeleted();
+            });
+    }
+
+}
